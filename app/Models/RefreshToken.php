53e60a2fe699b4cc7b92f0dd0f8b427d96422306
Vinicius Bueno <viniciusbuenodeoliveira2017@gmail.com>
2025-10-06 19:37:33 -0300

    feat(auth): Configura o Model de RefreshToken

--- app/Models/RefreshToken.php
+++ app/Models/RefreshToken.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class RefreshToken extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'token',
+        'expires_at',
+    ];
+
+    protected $casts = [
+        'expires_at' => 'datetime',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
